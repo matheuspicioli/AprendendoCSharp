d9486d4a138d10313ffe32efb6d678888bce2bc6
Matheus Picioli <matheus.picioli98@gmail.com>
2018-11-28 17:03:46 -0200

    Primeiro commit

---
+++ CriandoVariaveis/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int idade;
+            idade = 32;
+            
+            Console.WriteLine(idade);
+        }
+    }
+}
---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Project in Rider... YAYYYYYYY");
+            Console.WriteLine("A aplicação terminou");
+            Console.ReadLine();
+        }
+    }
+}
