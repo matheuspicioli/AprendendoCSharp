fb5297c12f4edd0e201a17bd6ebd0f0a050943c2
Matheus Picioli <matheus.picioli98@gmail.com>
2018-11-29 11:53:40 -0200

    2 aulas

---
+++ ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 4!!!");
+            double salario = 1200.50;
+            int salarioInteiro  = (int)salario;
+            Console.WriteLine(salarioInteiro);
+            long idade = 13000000000;
+
+            short quantidadeProdutos = 15000;
+            Console.WriteLine(quantidadeProdutos);
+
+            float altura = 1.75f;
+            Console.WriteLine(altura);
+        }
+    }
+}
---
+++ VariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, valor;
+            Console.WriteLine("Ponto flutuante");
+            salario = 1200.50;
+            Console.WriteLine(salario);
+
+            valor = 15 / 2;
+            Console.WriteLine("15 / 2 = " + valor);
+
+            valor = 15.0 / 2;
+            Console.WriteLine("15.0 / 2 = " + valor);
+        }
+    }
+}
